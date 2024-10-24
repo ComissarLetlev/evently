102bb749068906fc0bf25e648992b565e6931d44
Sagnik Biswas <89531647+ComissarLetlev@users.noreply.github.com>
2024-10-24 18:49:50 +0530

    first commit

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,18 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
-  clerkId: string;
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-  photo: string;
+  clerkId: string | undefined;
+  firstName: string | undefined;
+  lastName: string | undefined;
+  username: string | undefined;
+  email: string | undefined;
+  photo: string | undefined;
 };
 
 export type UpdateUserParams = {
-  firstName: string;
-  lastName: string;
-  username: string;
-  photo: string;
+  firstName: string | undefined;
+  lastName: string | undefined;
+  username: string | undefined;
+  photo: string | undefined;
 };
 
 // ====== EVENT PARAMS
